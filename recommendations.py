ab16c8774ae3e3a2fed96c2f4334ba9688b9393c
diaodiao <yuanpengxiong@foxmail.com>
2015-02-03 11:28:22 +0800

    user filtering

--- recommendations.py
+++ recommendations.py
@@ -63,26 +63,6 @@ def getRecommendations(prefs,person,similarity = sim_pearson):
 	rankings.sort();
 	rankings.reverse();
 	return rankings;
-	 	
-'''
-def getrecommendations(prefs,person,similarity=simPearson):
-	totals = {};
-	simSums = {};
-	for other in prefs:
-		if other == person:continue;
-		sim = similarity(prefs,person,other);
-		if sim == 0:continue;
-		for item in prefs[other]:
-			if item not in prefs[person] or prefs[person][item]==0:
-				totals.setdefault(item,0);
-				totals[item]+=sim*prefs[other][item];
-				simSums.setdefault(item,0);
-				simSums[item]+=sim;
-	rankings = [(totals[item]/simSums[item],item) for item in totals];
-	rankings.sort();
-	rankings.reverse();
-	return rankings;
-'''
 
 def transformPrefs(prefs):
 	result = {};
@@ -91,3 +71,30 @@ def transformPrefs(prefs):
 			result.setdefault(item,{});
 			result[item][person] = prefs[person][item];
 	return result;
+
+def calculateSimilarItems(prefs,n = 10):
+	result = {};
+	itemPrefs = transformPrefs(prefs);
+	c = 0;
+	for item in itemPrefs:
+		c+=1;
+		if c%100 ==0:print '%d / %d'%(c,len(itemPrefs))
+		scores = topMatches(itemPrefs,item,n = n,similarity = sim_distance);
+		result[item] = scores;
+	return result;
+
+def getRecommendedItems(prefs,itemMatch,user):
+	userRatings=prefs[user];
+	scores={};
+	totalSim ={};
+	for (item,rating) in userRatings.items():
+		for (similarity,item2) in itemMatch[item]:
+			if item2 in userRatings:continue;
+			scores.setdefault(item2,0);
+			scores[item2]+=similarity*rating;
+			totalSim.setdefault(item2,0);
+			totalSim[item2]+=similarity;
+	rankings = [(score/totalSim[item],item) for item,score in scores.items()];
+	rankings.sort();
+	rankings.reverse();
+	return rankings;
